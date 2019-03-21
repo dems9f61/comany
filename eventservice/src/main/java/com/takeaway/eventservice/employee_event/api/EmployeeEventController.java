988d496df6701cff1dfac3695dd488f3753dad61
StMinko <stephan.minko@nexnet.de>
2019-03-21 01:32:52 +0100

    Create and fill the readme with content

--- eventservice/src/main/java/com/takeaway/eventservice/employee_event/api/EmployeeEventController.java
+++ eventservice/src/main/java/com/takeaway/eventservice/employee_event/api/EmployeeEventController.java
@@ -4,15 +4,12 @@ import com.takeaway.eventservice.ApiVersions;
 import com.takeaway.eventservice.employee_event.service.EmployeeEventService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
-import java.net.HttpURLConnection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -39,8 +36,6 @@ public class EmployeeEventController
 
     @ApiOperation(value = "Retrieves all events related to an employee in ascending order")
     @GetMapping("/{uuid}")
-    @ApiResponses({
-            @ApiResponse(code = HttpURLConnection.HTTP_NOT_FOUND, message = "Could not find employee by the specified uuid!") })
     @ResponseStatus(HttpStatus.OK)
     public List<EmployeeEventResponse> getAllEmployeeEvents(@NotBlank @PathVariable("uuid") String uuid)
     {
