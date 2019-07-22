d44d7e15a8e430a5abf4a0b398c3fd5a51d14925
StMinko <stephan.minko@nexnet.de>
2019-07-22 15:36:49 +0200

    polish the code

--- employeeservice/src/test/java/com/takeaway/employeeservice/IntegrationTestSuite.java
+++ employeeservice/src/test/java/com/takeaway/employeeservice/IntegrationTestSuite.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -29,6 +30,7 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT,
         classes = { EmployeeServiceApplication.class })
+@ActiveProfiles("LOCAL")
 public abstract class IntegrationTestSuite
 {
     // =========================== Class Variables ===========================
