fc581f7675be0c0b6b431f02c3565c0567227cde
Stéphan Minko <stephan.minko@nexnet.de>
2019-08-07 15:27:31 +0200

    introduce liquibase

--- employeeservice/src/main/java/com/takeaway/employeeservice/EmployeeServiceApplication.java
+++ employeeservice/src/main/java/com/takeaway/employeeservice/EmployeeServiceApplication.java
@@ -3,11 +3,14 @@ package com.takeaway.employeeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EmployeeServiceApplication
 {
     public static void main(String[] args)
     {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
 }
